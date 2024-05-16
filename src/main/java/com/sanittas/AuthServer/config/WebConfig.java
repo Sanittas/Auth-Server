a837fa3f3bec39c14d297064f8fced2738000018
gabrielfernando23 <gabriel.ssilva@sptech.school>
2024-05-16 14:57:43 -0300

    abcd

--- src/main/java/com/sanittas/AuthServer/config/WebConfig.java
+++ src/main/java/com/sanittas/AuthServer/config/WebConfig.java
@@ -2,15 +2,17 @@ package com.sanittas.AuthServer.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+        registry.addMapping("/**");
     }
 }
