7db94228219468fc049ff54bd91c69a9b2729f00
gabrielfernando23 <gabriel.ssilva@sptech.school>
2024-06-16 21:13:05 -0300

    https

--- src/main/java/com/sanittas/AuthServer/services/SecurityConfigurer.java
+++ src/main/java/com/sanittas/AuthServer/services/SecurityConfigurer.java
@@ -39,7 +39,7 @@ public class SecurityConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://100.28.237.209")
+                .allowedOrigins("http://100.28.237.209", "https://sanittas.zapto.org")
 //                .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
